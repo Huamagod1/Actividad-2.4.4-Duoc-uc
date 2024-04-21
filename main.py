1809cdb7536d93ccb838ccd064e8e674c3e99a4a
Huamagod1 <137719719+Huamagod1@users.noreply.github.com>
2024-04-21 18:05:58 -0400

    Ejercicio completado

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ os.system("cls")
 #Prueba para ver como se comporta el modo remoto
 
 #Ejercicio de los Buses
+
+#1ero Partir preguntando 
+#Pedir el precio de cada pasaje
+#Si se ingresa un valor que no es un numero te indica que neesitas proporcionar un valor numerico valido
+totalIngresos=0
+
+while True:
+ try:
+       cantidad = int(input("Ingrese cantidad de pasajes que necesita\n"))
+
+       for contador in range(cantidad):
+        while True:
+          try:
+             precio = int(input(f"Favor indicar el precio del pasaje n° {contador+1} \n"))
+             totalIngresos = totalIngresos + precio
+             break
+          except:
+            print("Precio no existe")
+       break
+ except:
+            print("Opcion no valida")
+
+print(f"El Valor total a pagar por la cantidad de pasajes {cantidad} es de: {totalIngresos}")
